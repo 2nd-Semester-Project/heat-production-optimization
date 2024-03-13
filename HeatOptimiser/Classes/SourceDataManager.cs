e875b82805ad4fdf14283e2a1bc648f44c3bdce8
Linards <linardspakalns@gmail.com>
2024-03-13 17:53:16 +0100

    Early SourceData manager and interface

--- HeatOptimiser/Classes/SourceDataManager.cs
+++ HeatOptimiser/Classes/SourceDataManager.cs
@@ -1,10 +1,34 @@
+using System.IO;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Microsot.Interop.Excel;
+
 namespace HeatOptimiser
 {
-    public class SourceDataManager : ISourceDataManager
+    public class SourceDataManager : ISourceDataManager 
     {
-        public SourceDataManager()
+        private string FilePath;
+        public List<string> SummerData = new List<string>();
+        public List<string> WinterData = new List<string>();
+
+
+        public SourceDataManager(string filePath)
         {
-            throw new System.NotImplementedException();
+            FilePath = filePath;
         }
+        
+        public List<String> SummerData (string? filePath)
+        {
+            public string filePath = "HeatOptimiser/2024 Heat Production Optimization - Danfoss Deliveries - Source Data Manager.xlsx";
+
+
+            return SummerData;
+        }
+       public List<string> WinterData(string? filePath)
+        {
+           
+
+            return electricityPriceData;
+        } 
+
     }
 }
--- HeatOptimiser/Interfaces/ISourceDataManager.cs
+++ HeatOptimiser/Interfaces/ISourceDataManager.cs
@@ -2,6 +2,7 @@ namespace HeatOptimiser
 {
     public interface ISourceDataManager
     {
-
+        List<String> SummerData (string? filePath); 
+        List<String> WinterData (string? filePath);
     }
 }

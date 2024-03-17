cabed95914b466af92e29c4f2bcd5ce502ea4e8a
Pauls <pauls2odins@gmail.com>
2024-03-17 21:22:35 +0100

    Implemented Optimiser

--- HeatOptimiser/Program.cs
+++ HeatOptimiser/Program.cs
@@ -3,7 +3,34 @@
     class Program {
         public static void Main()
         {
+            AssetManager assets = new();
+            assets.AddUnit("GB", "none", 5.0, 0, 1.1, 500, 215);
+            assets.AddUnit("OB", "none", 4.0, 0, 1.2, 700, 265);
+
+            SourceDataManager dataManager = new();
+            Optimiser optimiser = new(dataManager, assets);
+            string startDateStr = "12/02/2023";
+            string endDateStr = "25/02/2023";
+            DateTime startDate = DateTime.ParseExact(startDateStr, "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture);
+            DateTime endDate = DateTime.ParseExact(endDateStr, "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture);
+            Schedule optimisedData = optimiser.Optimise(startDate, endDate);
+
+            foreach (ScheduleHour hour in optimisedData.schedule)
+            {
+                Console.WriteLine(hour.Hour);
+                foreach (ProductionAsset asset in hour.Assets)
+                {
+                    Console.Write($"{asset.Name} ");
+                }
+                Console.WriteLine();
+                foreach (double demand in hour.Demands)
+                {
+                    Console.Write($"{demand} ");
+                }
+                Console.WriteLine("\n");
+            }
+
             new TextBasedUI().example();
-        }   
+        }
     }
 }

1061190f7991441d85e94ca044a3f24ef3fffc5a
Linards <linardspakalns@gmail.com>
2024-04-30 21:57:24 +0200

    Add ResultsViewModel.cs and remove ResultsViewModels.cs and update ResultsView.axaml to use the new ResultsViewModel

---
+++ HeatOptimiser/UserInterface/ViewModels/ResultsViewModel.cs
@@ -0,0 +1,21 @@
+using System;
+using ReactiveUI;
+using CommunityToolkit.Mvvm;
+using LiveChartsCore;
+using LiveChartsCore.Defaults;
+using LiveChartsCore.SkiaSharpView;
+using HeatOptimiser;
+
+namespace UserInterface.ViewModels;
+
+public class ResultsViewModel : ViewModelBase
+{
+    public ISeries[] Series { get; set; } = new ISeries[]
+    {
+        new LineSeries<double>
+        {
+            Values = new double[] { 2, 1, 3, 5, 3, 4, 6 },
+            Fill = null
+        }
+    };
+}

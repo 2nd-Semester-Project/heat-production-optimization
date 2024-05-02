128b2828cae91f4122a35a2e893534bab48e2dcb
Linards <linardspakalns@gmail.com>
2024-05-02 17:04:32 +0200

    Refactor ResultsView.axaml to remove unused bindings
    First working display of source data

--- HeatOptimiser/UserInterface/Classes/DataVisualizer.cs
+++ HeatOptimiser/UserInterface/Classes/DataVisualizer.cs
@@ -1,26 +1,44 @@
 using System;
 using System.Collections.Generic;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace HeatOptimiser
 {
 
-    // public class DataVisualizer
-    // {
+    public class DataVisualizer
+{
+    public SourceData sourceData = new SourceData(); // This already loads your summer and winter data.
 
-    //     private ResultsDataManager resultsDataManager;
+    public List<DateTime> Times = new List<DateTime>(); // For X-axis
+    public List<double?> HeatDemands = new List<double?>(); // For Y-axis (heat demand)
+    public List<double?> ElectricityPrices = new List<double?>(); // For Y-axis (electricity price)
 
-    //     public void SetResultsDataManager(ResultsDataManager resultsDataManager)
-    //     {
-    //         this.resultsDataManager = resultsDataManager;
-    //     }
+    public void AccessSummerData()
+    {
+        // Accessing the summer data
+        foreach (var point in sourceData.SummerData)
+        {
+            if (point.TimeFrom.HasValue) // Ensuring the time is not null
+            {
+                Times.Add(point.TimeFrom.Value);
+                HeatDemands.Add(point.HeatDemand);
+                ElectricityPrices.Add(point.ElectricityPrice);
+            }
+        }
+    }
 
-    //     public void AccessResults()
-    //     {
-    //         // Now you can access filePath and am using the resultsDataManager instance
-    //         string filePath = resultsDataManager.passedFilePath;
-    //         AssetManager am = resultsDataManager.am;
-
-    //         // Use filePath and am as needed
-    //     }
-    // }
+    public void AccessWinterData()
+    {
+        // Accessing the winter data
+        foreach (var point in sourceData.WinterData)
+        {
+            if (point.TimeFrom.HasValue) // Ensuring the time is not null
+            {
+                Times.Add(point.TimeFrom.Value);
+                HeatDemands.Add(point.HeatDemand);
+                ElectricityPrices.Add(point.ElectricityPrice);
+            }
+        }
+    }
+}
 }
--- HeatOptimiser/UserInterface/ViewModels/ResultsViewModel.cs
+++ HeatOptimiser/UserInterface/ViewModels/ResultsViewModel.cs
@@ -1,22 +1,127 @@
 using System;
+using System.Linq;
 using ReactiveUI;
 using CommunityToolkit.Mvvm;
 using LiveChartsCore;
 using LiveChartsCore.Defaults;
 using LiveChartsCore.SkiaSharpView;
 using HeatOptimiser;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Reactive.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace UserInterface.ViewModels;
 
 public class ResultsViewModel : ViewModelBase
 {
-    
-    public ISeries[] Series { get; set; } = new ISeries[]
+    private readonly DataVisualizer dataVisualizer = new DataVisualizer();
+
+    // public ObservableCollection<ObservableValue> WinterHeatDemandData = new ObservableCollection<ObservableValue>();
+    // public ISeries[] Series {get; set;}
+
+
+    // public ObservableCollection<ObservablePoint> WinterHeatDemandData { get; set; } = new ObservableCollection<ObservablePoint>();
+
+
+    // public Axis[] XAxes { get; set; } =
+    // {
+    //     new Axis
+    //     {
+    //     LabelsRotation = 15,
+    //     Labeler = value =>
+    //     {
+    //     try 
+    //     {
+    //         return new DateTime((long)value).ToString("MM-dd HH");
+    //     }
+    //     catch (Exception ex)
+    //     {
+    //         Console.WriteLine($"Error formatting date: {ex}");
+    //         return "Invalid date";
+    //     }
+    //     },
+    //         UnitWidth = TimeSpan.FromDays(1).Ticks // Sets the unit width to a day
+    //     }
+    // };
+
+    // public Axis[] YAxes { get; set; } =
+    // {
+    //     new Axis
+    //     {
+    //         Name = "Heat Demand (kW)",
+    //     }
+    // };
+
+    // public ResultsViewModel()
+    // {
+    //     dataVisualizer.AccessWinterData(); // Load winter data
+
+    //     foreach (var point in dataVisualizer.sourceData.WinterData)
+    //     {
+    //         if (point.HeatDemand.HasValue)
+    //         {
+    //             WinterHeatDemandData.Add(new ObservableValue(point.HeatDemand.Value));
+    //         }
+
+    //     };
+
+
+    // }
+
+
+    // public void Initialize()
+    // {
+    //     WinterHeatDemandData.Add(new ObservableValue(1));
+    //     WinterHeatDemandData.Add(new ObservableValue(2));
+    //     WinterHeatDemandData.Add(new ObservableValue(3));
+    //     WinterHeatDemandData.Add(new ObservableValue(4));
+    //     WinterHeatDemandData.Add(new ObservableValue(5));
+    //     WinterHeatDemandData.Add(new ObservableValue(6));
+
+    //     Series = new ISeries[]
+    //     {
+    //         new LineSeries<ObservableValue>
+    //         {
+    //             Values = WinterHeatDemandData,
+    //             // DataLabels = true,
+    //             // LabelPoint = point => point.Value.ToString("N2"),
+    //             // ScalesXAt = 0,
+    //             // ScalesYAt = 0
+    //         }
+    //     };
+    // }
+
+
+
+
+
+    private readonly Random _random = new();
+    private readonly ObservableCollection<ObservableValue> WinterHeatDemandData;
+    public ObservableCollection<ISeries> Series { get; set; }
+
+    public ResultsViewModel()
     {
-        new LineSeries<double>
+        // Use ObservableCollections to let the chart listen for changes (or any INotifyCollectionChanged). 
+        WinterHeatDemandData = new ObservableCollection<ObservableValue>();
+
+        foreach (var point in dataVisualizer.sourceData.WinterData)
         {
-            Values = new double[] { 2, 1, 3, 5, 3, 4, 6 },
-            Fill = null
+            if (point.HeatDemand.HasValue)
+            {
+                WinterHeatDemandData.Add(new ObservableValue(point.HeatDemand.Value));
+            }
         }
-    };
+
+
+        Series = new ObservableCollection<ISeries>
+        {
+            new LineSeries<ObservableValue>
+            {
+                Values = WinterHeatDemandData,
+                Fill = null
+            }
+        };
+    }
 }
+

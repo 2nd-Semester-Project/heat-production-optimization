adfc54441f40b0bb93514bfc68b6c6ebe98595b7
svenons <gotvonisvens@gmail.com>
2024-05-16 00:43:20 +0200

    validation

--- HeatOptimiser/Classes/SourceDataManager.cs
+++ HeatOptimiser/Classes/SourceDataManager.cs
@@ -64,50 +64,52 @@ namespace HeatOptimiser
         public static List<SourceDataPoint> LoadXLSXFile(string file, int rowStart, int columnStart, int workSheetNumber = 0)
         {
             var sourceList = new List<SourceDataPoint>();
-
-            ExcelPackage.LicenseContext = LicenseContext.NonCommercial; // EPPlus license
-
-            using (var package = new ExcelPackage(new FileInfo(file)))
+            if (file != string.Empty || File.Exists(file) || rowStart >= 1 || columnStart >= 1)
             {
-                ExcelWorksheet worksheet = null ?? package.Workbook.Worksheets[0];
-                try
-                {
-                    worksheet = package.Workbook.Worksheets[workSheetNumber];
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine($"Worksheet not found: {e}");
-                    return sourceList;
-                }
+                ExcelPackage.LicenseContext = LicenseContext.NonCommercial; // EPPlus license
 
-                if (worksheet.Dimension == null)
+                using (var package = new ExcelPackage(new FileInfo(file)))
                 {
-                    Console.WriteLine("The worksheet is empty.");
-                    return sourceList;
-                }
-
-                for (int row = rowStart; row <= worksheet.Dimension.End.Row; row++)
-                {
+                    ExcelWorksheet worksheet;
                     try
                     {
-                        DateTime temp;
-                        string[] formats = { "dd/MM/yyyy HH.mm.ss", "dd/MM/yyyy HH:mm:ss", "HH.mm.ss", "HH:mm:ss" };
-                        SourceDataPoint sourceData = new SourceDataPoint
-                        {
-                            TimeFrom = DateTime.TryParseExact(worksheet.Cells[row, columnStart].Value?.ToString(), formats, CultureInfo.InvariantCulture, DateTimeStyles.None, out temp) ? temp : (DateTime?)null,
-                            TimeTo = DateTime.TryParseExact(worksheet.Cells[row, columnStart + 1].Value?.ToString(), formats, CultureInfo.InvariantCulture, DateTimeStyles.None, out temp) ? temp : (DateTime?)null,
-                            HeatDemand = worksheet.Cells[row, columnStart + 2]?.Value != null ? double.Parse(worksheet.Cells[row, columnStart + 2].Value.ToString()!) : null,
-                            ElectricityPrice = worksheet.Cells[row, columnStart + 3]?.Value != null ? double.Parse(worksheet.Cells[row, columnStart + 3].Value.ToString()!) : null
-                        };
-                        sourceList.Add(sourceData);
+                        worksheet = null ?? package.Workbook.Worksheets[0];
+                        worksheet = package.Workbook.Worksheets[workSheetNumber];
                     }
                     catch (Exception e)
                     {
-                        Console.WriteLine($"Error: {e}");
+                        Console.WriteLine($"Worksheet not found: {e}");
+                        return sourceList;
+                    }
+
+                    if (worksheet.Dimension == null)
+                    {
+                        Console.WriteLine("The worksheet is empty.");
+                        return sourceList;
+                    }
+
+                    for (int row = rowStart; row <= worksheet.Dimension.End.Row; row++)
+                    {
+                        try
+                        {
+                            DateTime temp;
+                            string[] formats = { "dd/MM/yyyy HH.mm.ss", "dd/MM/yyyy HH:mm:ss", "HH.mm.ss", "HH:mm:ss" };
+                            SourceDataPoint sourceData = new SourceDataPoint
+                            {
+                                TimeFrom = DateTime.TryParseExact(worksheet.Cells[row, columnStart].Value?.ToString(), formats, CultureInfo.InvariantCulture, DateTimeStyles.None, out temp) ? temp : (DateTime?)null,
+                                TimeTo = DateTime.TryParseExact(worksheet.Cells[row, columnStart + 1].Value?.ToString(), formats, CultureInfo.InvariantCulture, DateTimeStyles.None, out temp) ? temp : (DateTime?)null,
+                                HeatDemand = worksheet.Cells[row, columnStart + 2]?.Value != null ? double.Parse(worksheet.Cells[row, columnStart + 2].Value.ToString()!) : null,
+                                ElectricityPrice = worksheet.Cells[row, columnStart + 3]?.Value != null ? double.Parse(worksheet.Cells[row, columnStart + 3].Value.ToString()!) : null
+                            };
+                            sourceList.Add(sourceData);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine($"Error: {e}");
+                        }
                     }
                 }
             }
-
             return sourceList;
         }
 

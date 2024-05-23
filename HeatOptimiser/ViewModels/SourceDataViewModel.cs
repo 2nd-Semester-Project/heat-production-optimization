1b7404ba4a04496a7fdd728de9224b63452db0ca
Linards <linardspakalns@gmail.com>
2024-05-23 13:42:09 +0200

    Changed the data graph to be in source data

--- HeatOptimiser/ViewModels/SourceDataViewModel.cs
+++ HeatOptimiser/ViewModels/SourceDataViewModel.cs
@@ -1,6 +1,13 @@
 using ReactiveUI;
 using System.Reactive;
 using HeatOptimiser;
+using System.Collections.ObjectModel;
+using LiveChartsCore.Defaults;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView.Painting;
+using SkiaSharp;
+using System;
 
 namespace UserInterface.ViewModels
 {
@@ -15,6 +22,20 @@ namespace UserInterface.ViewModels
         private bool _isErrorSelectRowVisible;
         private string _errorSelectColumn;
         private bool _isErrorSelectColumnVisible;
+        private string _sourceText;
+        private readonly ObservableCollection<DateTimePoint> _heatDemandData;
+        private readonly ObservableCollection<DateTimePoint> _electricityPriceData;
+       
+
+        public ObservableCollection<ISeries> Series { get; set; }
+
+        public Axis[] XAxes { get; set; }
+        public Axis[] YAxes { get; set; }
+        public string SourceText
+        {
+            get => _sourceText;
+            set => this.RaiseAndSetIfChanged(ref _sourceText, value);
+        }
 
         public ReactiveCommand<Unit, Unit> SourceDataCommand { get; }
 
@@ -87,6 +108,78 @@ namespace UserInterface.ViewModels
             IsErrorSelectRowVisible = false;
             ErrorSelectColumn = string.Empty;
             IsErrorSelectColumnVisible = false;
+
+            _heatDemandData = new ObservableCollection<DateTimePoint>();
+            _electricityPriceData = new ObservableCollection<DateTimePoint>();
+            _sourceText = "Source Data not loaded. \nPlease load the data.";
+
+            if (SettingsManager.GetSetting("DataLoaded") == "True")
+            {
+                _sourceText = "Source Data loaded.";
+                foreach (var point in DataVisualizer.sourceData.LoadedData)
+                {
+                    if (point.HeatDemand.HasValue && point.TimeFrom.HasValue && point.ElectricityPrice.HasValue)
+                    {
+                        _heatDemandData.Add(new DateTimePoint(point.TimeFrom.Value, point.HeatDemand.Value));
+                        _electricityPriceData.Add(new DateTimePoint(point.TimeFrom.Value, point.ElectricityPrice.Value));
+
+                        Console.WriteLine($"Added Point: TimeFrom={point.TimeFrom.Value}, HeatDemand={point.HeatDemand.Value}, ElectricityPrice={point.ElectricityPrice.Value}");
+                    }
+                }
+            }
+
+            Series = new ObservableCollection<ISeries>
+            {
+                new LineSeries<DateTimePoint>
+                {
+                    Values = _heatDemandData,
+                    Name = "Heat Demand (MWh)",
+                    Fill = null,
+                    GeometryStroke = null,
+                    GeometryFill = null,
+                    LineSmoothness = 1,
+                    Stroke = new SolidColorPaint(new SKColor(194, 36, 62))
+                    {
+                        StrokeThickness = 3 // Set the thickness of the Heat Demand line
+                    }
+                },
+                new LineSeries<DateTimePoint>
+                {
+                    Values = _electricityPriceData,
+                    Name = "Electricity Price (€/MWh)",
+                    Fill = null,
+                    GeometryStroke = null,
+                    GeometryFill = null,
+                    LineSmoothness = 2,
+                    ScalesYAt = 1, // This tells the series to use the secondary Y-axis
+                   Stroke = new SolidColorPaint(new SKColor(0, 92, 230)) // Set the color shade 
+                    {
+                        StrokeThickness = 3 // Set the thickness of the Electricity Price line
+                    },
+                }
+            };
+
+              XAxes = new Axis[]
+            {
+                new DateTimeAxis(TimeSpan.FromDays(1), date => date.ToString("MMMM dd HH:mm"))
+            };
+
+            YAxes = new Axis[]
+            {
+                new Axis
+                {
+                    Name = "Heat Demand (MWh)",
+                    TextSize = 16,
+                    NameTextSize = 18
+                },
+                new Axis
+                {
+                    Name = "Electricity Price (€/MWh)",
+                    TextSize = 16,
+                    NameTextSize = 18,
+                    Position = LiveChartsCore.Measure.AxisPosition.End
+                }
+            };
         }
 
         private void LoadSourceData()
@@ -134,5 +227,7 @@ namespace UserInterface.ViewModels
                 sourceData.LoadSourceData(SelectedFilePath, SelectedRow, SelectedColumn);
             }
         }
+
+        
     }
 }
